4b2e24ea2199b9a3f960e14a1afa03b9c1fcc76b
KevalBhattSW <keval.bhatt@swgroup.com>
2026-06-23 16:52:03 +0100

    Update Import-TextFilesToSql.ps1

--- PowerShell/Import-TextFilesToSql.ps1
+++ PowerShell/Import-TextFilesToSql.ps1
@@ -42,7 +42,120 @@ param(
 )
 
 $ErrorActionPreference = "Stop"
+Add-Type @"
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
 
+public class CsvDataReader : IDataReader
+{
+    private readonly StreamReader  _reader;
+    private readonly char          _delimiter;
+    private readonly string        _sourceFile;
+    private readonly DateTime      _loadDt;
+    private readonly string[]      _headers;
+    private string[]               _current;
+    private bool                   _closed;
+
+    private int DataColCount  { get { return _headers.Length; } }
+    private int TotalColCount { get { return _headers.Length + 2; } }
+
+    public CsvDataReader(string filePath, char delimiter)
+    {
+        _reader     = new StreamReader(filePath, System.Text.Encoding.UTF8, true);
+        _delimiter  = delimiter;
+        _sourceFile = Path.GetFileName(filePath);
+        _loadDt     = DateTime.UtcNow;
+
+        var headerLine = _reader.ReadLine();
+        _headers = headerLine == null
+            ? new string[0]
+            : headerLine.Split(_delimiter);
+    }
+
+    public int RowCount { get; private set; }
+
+    public bool Read()
+    {
+        if (_closed) return false;
+        var line = _reader.ReadLine();
+        if (line == null) return false;
+        _current = line.Split(_delimiter);
+        RowCount++;
+        return true;
+    }
+
+    public int FieldCount { get { return TotalColCount; } }
+
+    public object GetValue(int i)
+    {
+        if (i < DataColCount)
+        {
+            if (_current == null || i >= _current.Length) return DBNull.Value;
+            var v = _current[i];
+            return string.IsNullOrEmpty(v) ? (object)DBNull.Value : v;
+        }
+        if (i == DataColCount)     return _sourceFile;
+        if (i == DataColCount + 1) return _loadDt;
+        throw new IndexOutOfRangeException();
+    }
+
+    public string GetName(int i)
+    {
+        if (i < DataColCount)      return _headers[i];
+        if (i == DataColCount)     return "SourceFile";
+        if (i == DataColCount + 1) return "LoadDateTime";
+        throw new IndexOutOfRangeException();
+    }
+
+    public int GetOrdinal(string name)
+    {
+        for (int i = 0; i < _headers.Length; i++)
+            if (string.Equals(_headers[i], name, StringComparison.OrdinalIgnoreCase)) return i;
+        if (name == "SourceFile")   return DataColCount;
+        if (name == "LoadDateTime") return DataColCount + 1;
+        throw new IndexOutOfRangeException(name);
+    }
+
+    public void Close()               { _closed = true; _reader.Dispose(); }
+    public void Dispose()             { Close(); }
+    public int  Depth                 { get { return 0; } }
+    public bool IsClosed              { get { return _closed; } }
+    public int  RecordsAffected       { get { return -1; } }
+    public bool NextResult()          { return false; }
+    public DataTable GetSchemaTable() { return null; }
+
+    public bool     GetBoolean(int i)  { return Convert.ToBoolean(GetValue(i)); }
+    public byte     GetByte(int i)     { return Convert.ToByte(GetValue(i)); }
+    public long     GetBytes(int i, long fo, byte[] buf, int bo, int len) { return 0; }
+    public char     GetChar(int i)     { return Convert.ToChar(GetValue(i)); }
+    public long     GetChars(int i, long fo, char[] buf, int bo, int len) { return 0; }
+    public Guid     GetGuid(int i)     { return Guid.Parse(GetValue(i).ToString()); }
+    public short    GetInt16(int i)    { return Convert.ToInt16(GetValue(i)); }
+    public int      GetInt32(int i)    { return Convert.ToInt32(GetValue(i)); }
+    public long     GetInt64(int i)    { return Convert.ToInt64(GetValue(i)); }
+    public float    GetFloat(int i)    { return Convert.ToSingle(GetValue(i)); }
+    public double   GetDouble(int i)   { return Convert.ToDouble(GetValue(i)); }
+    public decimal  GetDecimal(int i)  { return Convert.ToDecimal(GetValue(i)); }
+    public DateTime GetDateTime(int i) { return Convert.ToDateTime(GetValue(i)); }
+    public string   GetString(int i)   { return GetValue(i) == null ? null : GetValue(i).ToString(); }
+    public string   GetDataTypeName(int i) { return "nvarchar"; }
+    public Type     GetFieldType(int i)    { return typeof(string); }
+
+    public int GetValues(object[] values)
+    {
+        int n = Math.Min(values.Length, TotalColCount);
+        for (int i = 0; i < n; i++) values[i] = GetValue(i);
+        return n;
+    }
+
+    public bool IsDBNull(int i) { return GetValue(i) == DBNull.Value; }
+
+    public object this[int i]    { get { return GetValue(i); } }
+    public object this[string n] { get { return GetValue(GetOrdinal(n)); } }
+}
+"@ -ReferencedAssemblies "System.Data"
 # ------------------------------------------------------------------
 # 0. Setup
 # ------------------------------------------------------------------
@@ -172,120 +285,7 @@ END
 # ------------------------------------------------------------------
 
 
-Add-Type @"
-    using System;
-    using System.Collections.Generic;
-    using System.Data;
-    using System.IO;
 
-    public class CsvDataReader : IDataReader
-    {
-        private readonly StreamReader  _reader;
-        private readonly char          _delimiter;
-        private readonly string        _sourceFile;
-        private readonly DateTime      _loadDt;
-        private readonly string[]      _headers;       // data columns from file header
-        private string[]               _current;
-        private bool                   _closed;
-
-        // Total columns = data columns + SourceFile + LoadDateTime
-        private int DataColCount  => _headers.Length;
-        private int TotalColCount => _headers.Length + 2;
-
-        public CsvDataReader(string filePath, char delimiter)
-        {
-            _reader     = new StreamReader(filePath, System.Text.Encoding.UTF8, true);
-            _delimiter  = delimiter;
-            _sourceFile = Path.GetFileName(filePath);
-            _loadDt     = DateTime.UtcNow;
-
-            var headerLine = _reader.ReadLine();
-            _headers = headerLine == null
-                ? Array.Empty<string>()
-                : headerLine.Split(_delimiter);
-        }
-
-        public int RowCount { get; private set; }
-
-        // IDataReader / IDataRecord -----------------------------------------
-        public bool Read()
-        {
-            if (_closed) return false;
-            var line = _reader.ReadLine();
-            if (line == null) return false;
-            _current = line.Split(_delimiter);
-            RowCount++;
-            return true;
-        }
-
-        public int FieldCount => TotalColCount;
-
-        public object GetValue(int i)
-        {
-            if (i < DataColCount)
-            {
-                if (_current == null || i >= _current.Length) return DBNull.Value;
-                var v = _current[i];
-                return string.IsNullOrEmpty(v) ? (object)DBNull.Value : v;
-            }
-            if (i == DataColCount)     return _sourceFile;  // SourceFile
-            if (i == DataColCount + 1) return _loadDt;      // LoadDateTime
-            throw new IndexOutOfRangeException();
-        }
-
-        public string GetName(int i)
-        {
-            if (i < DataColCount)      return _headers[i];
-            if (i == DataColCount)     return "SourceFile";
-            if (i == DataColCount + 1) return "LoadDateTime";
-            throw new IndexOutOfRangeException();
-        }
-
-        public int GetOrdinal(string name)
-        {
-            for (int i = 0; i < _headers.Length; i++)
-                if (string.Equals(_headers[i], name, StringComparison.OrdinalIgnoreCase)) return i;
-            if (name == "SourceFile")   return DataColCount;
-            if (name == "LoadDateTime") return DataColCount + 1;
-            throw new IndexOutOfRangeException(name);
-        }
-
-        // --- stubs required by IDataReader that we don't need ---
-        public void Close()  { _closed = true; _reader.Dispose(); }
-        public void Dispose(){ Close(); }
-        public int  Depth    => 0;
-        public bool IsClosed => _closed;
-        public int  RecordsAffected => -1;
-        public bool NextResult()    => false;
-        public DataTable GetSchemaTable() => null;
-
-        public bool    GetBoolean(int i)  => Convert.ToBoolean(GetValue(i));
-        public byte    GetByte(int i)     => Convert.ToByte(GetValue(i));
-        public long    GetBytes(int i, long fo, byte[] buf, int bo, int len) => 0;
-        public char    GetChar(int i)     => Convert.ToChar(GetValue(i));
-        public long    GetChars(int i, long fo, char[] buf, int bo, int len) => 0;
-        public Guid    GetGuid(int i)     => Guid.Parse(GetValue(i).ToString());
-        public short   GetInt16(int i)    => Convert.ToInt16(GetValue(i));
-        public int     GetInt32(int i)    => Convert.ToInt32(GetValue(i));
-        public long    GetInt64(int i)    => Convert.ToInt64(GetValue(i));
-        public float   GetFloat(int i)    => Convert.ToSingle(GetValue(i));
-        public double  GetDouble(int i)   => Convert.ToDouble(GetValue(i));
-        public decimal GetDecimal(int i)  => Convert.ToDecimal(GetValue(i));
-        public DateTime GetDateTime(int i)=> Convert.ToDateTime(GetValue(i));
-        public string  GetString(int i)   => GetValue(i)?.ToString();
-        public string  GetDataTypeName(int i) => "nvarchar";
-        public Type    GetFieldType(int i)    => typeof(string);
-        public int     GetValues(object[] values)
-        {
-            int n = Math.Min(values.Length, TotalColCount);
-            for (int i = 0; i < n; i++) values[i] = GetValue(i);
-            return n;
-        }
-        public bool IsDBNull(int i) => GetValue(i) == DBNull.Value;
-        public object this[int i]    => GetValue(i);
-        public object this[string n] => GetValue(GetOrdinal(n));
-    }
-"@ -ReferencedAssemblies "System.Data"
 
 Write-Host "`n=== Step 3: Staging already-unzipped files ===" -ForegroundColor Cyan
 
